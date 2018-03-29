c151ffd9065d7c0d23a2fc2593347fa9efc85aae
MuhammadUsamax123 <muhammadusamax123.com>
2018-03-29 23:34:26 +0500

    first

---
+++ WebApplication3/Controllers/cvController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class cvController : Controller
+    {
+        // GET: cv
+        public ActionResult cV()
+        {
+            return View();
+        }
+    }
+}
